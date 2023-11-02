769ee533e5bfdf0bb7c5f9e08085e61c9153599f
SAASMEEM <46927159+SAASMEEM@users.noreply.github.com>
2023-11-02 11:28:50 +0100

    print function

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -9,6 +9,10 @@ public class Application {
         }
     }
 
+    public void print(String bla) {
+        System.out.println(bla);
+    }
+
     public static void main(String... args) {
         Application application = new Application();
         System.out.println(application.getFibonacciNumberAt(5));
